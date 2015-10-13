ae6305082f8d6092f1fb6da6d9d02934ea220c0a
Tomer Amir <tomer.a@hotmail.com>
2015-10-13 16:54:46 +0200

    Final commit

--- module/sysax.c
+++ module/sysax.c
@@ -2,21 +2,16 @@
 
 int check_sysax_attack(const char *data) {
 	char *location, *param_end;
-	
-	printk(KERN_INFO "here\n%s", data);
-
 	if (strstr(data, "pid=mk_folder2_name1.htm")) {
 		// Create folder attempt -> check if legitimate
 		location = strstr(data, "name=\"e2\"");
 
-		printk(KERN_INFO "found pid\n");
-
 		if (location != NULL) {
 			// Found vulnerable parameter, check if attacked
 			param_end = strstr(location, "--");
-			printk(KERN_INFO "found e2 param\n");
 
 			if (param_end - location > XP_FOLDER_NAME_LEN) {
+				printk(KERN_INFO "Sysax attack detected, Comutication blocked.\n");
 				return 1;
 			}
 		}
